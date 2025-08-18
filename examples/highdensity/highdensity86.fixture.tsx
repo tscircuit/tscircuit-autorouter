a8cdadbd19c90227254075eed9d69eb362ba6aba
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-08-18 16:16:17 +0200

    Added autorouter highdensity repro

---
+++ examples/highdensity/highdensity86.fixture.tsx
@@ -0,0 +1,8 @@
+import cn899 from "examples/assets/cn899-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn899.nodeWithPortPoints} />
+  )
+}
