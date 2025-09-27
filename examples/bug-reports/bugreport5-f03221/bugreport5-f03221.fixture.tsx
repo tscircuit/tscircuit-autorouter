9001ff8b9a2651a550f4ff4d22c34102c7c4aa51
imrishabh18 <belikerishabh@gmail.com>
2025-09-28 01:40:54 +0530

    format

--- examples/bug-reports/bugreport5-f03221/bugreport5-f03221.fixture.tsx
+++ examples/bug-reports/bugreport5-f03221/bugreport5-f03221.fixture.tsx
@@ -3,5 +3,5 @@ import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebu
 import bugReportJson from "./bugreport5-f03221.json"
 
 export default () => {
-  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />;
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
 }
