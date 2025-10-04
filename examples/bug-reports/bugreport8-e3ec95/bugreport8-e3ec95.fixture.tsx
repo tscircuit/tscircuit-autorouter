314934b9c5c240b099222fd8cb852a8e1dc874af
Severin Ibarluzea <seve700@gmail.com>
2025-10-04 14:17:35 -0700

    Add bug report fixture 8-e3ec95

---
+++ examples/bug-reports/bugreport8-e3ec95/bugreport8-e3ec95.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport8-e3ec95.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
