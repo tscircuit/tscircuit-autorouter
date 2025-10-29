81a2272e3134941a8942ddb37b37d4f7137f8b77
seveibar <seveibar@gmail.com>
2025-10-28 22:18:31 -0700

    add docstring describing variation for mesh node solver

--- lib/solvers/AssignableViaAutoroutingPipeline/CapacityMeshNodeSolver_OnlyTraverseLayersInAssignableObstacles.ts
+++ lib/solvers/AssignableViaAutoroutingPipeline/CapacityMeshNodeSolver_OnlyTraverseLayersInAssignableObstacles.ts
@@ -5,7 +5,23 @@ interface CapacityMeshNodeSolverOptions {
   capacityDepth?: number
 }
 
+/**
+ * This capacity mesh node solver is meant to be used in contexts where vias
+ * aren't allowed, but there may be assignable vias on the PCB as obstacles.
+ *
+ * The behavior is different than other Capacity Mesh Node solvers in that:
+ * - If there is no obstacle, instead of having a multi-layer capacity mesh node
+ *   we have two single-layer capacity mesh nodes
+ * - If we have an "assignable" obstacle, i.e. an obstacle with "netIsAssignable"
+ *   set to true we create a multi-layer capacity mesh node with a capacity of
+ *   1 (a single via)
+ *   - NOTE: There is only ONE multi-layer capacity mesh node per assignable obstacle
+ * - Mesh nodes should not be "too big", if a capacity mesh node is more than
+ *   MAX_SIZE_FOR_SINGLE_LAYER_NODES it will be split into 4 smaller nodes
+ */
 export class CapacityMeshNodeSolver_OnlyTraverseLayersInAssignableObstacles extends CapacityMeshNodeSolver2_NodeUnderObstacle {
+  MAX_SIZE_FOR_SINGLE_LAYER_NODES = 4 // 4x4mm
+
   constructor(
     public srj: SimpleRouteJson,
     public opts: CapacityMeshNodeSolverOptions = {},
