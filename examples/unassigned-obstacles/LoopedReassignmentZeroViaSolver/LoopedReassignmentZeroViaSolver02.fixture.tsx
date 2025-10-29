94b245834cb3540a9cd14b8497dd789863b1b913
seveibar <seveibar@gmail.com>
2025-10-28 18:12:41 -0700

    configure fixture to make vias assignable

--- examples/unassigned-obstacles/LoopedReassignmentZeroViaSolver/LoopedReassignmentZeroViaSolver02.fixture.tsx
+++ examples/unassigned-obstacles/LoopedReassignmentZeroViaSolver/LoopedReassignmentZeroViaSolver02.fixture.tsx
@@ -1,12 +1,27 @@
 import { GenericSolverDebugger } from "lib/testing/GenericSolverDebugger"
 import { LoopedReassignmentZeroViaSolver } from "lib/solvers/LoopedReassignmentZeroViaSolver/LoopedReassignmentZeroViaSolver"
-import simpleRouteJson from "./LoopedReassignmentZeroViaSolver01.json"
+import simpleRouteJson from "./LoopedReassignmentZeroViaSolver02.json"
+import { SimpleRouteJson } from "lib/types"
+
+function makeViasAssignable(srj: SimpleRouteJson) {
+  return {
+    ...srj,
+    obstacles: srj.obstacles.map((obstacle) => ({
+      ...obstacle,
+      netIsAssignable:
+        obstacle.connectedTo.length === 0 && obstacle.layers.length === 2,
+    })),
+  }
+}
 
 export default () => {
   return (
     <GenericSolverDebugger
       createSolver={() =>
-        new LoopedReassignmentZeroViaSolver(simpleRouteJson, {})
+        new LoopedReassignmentZeroViaSolver(
+          makeViasAssignable(simpleRouteJson as SimpleRouteJson),
+          {},
+        )
       }
       showDeepestVisualizationInitial={true}
     />
