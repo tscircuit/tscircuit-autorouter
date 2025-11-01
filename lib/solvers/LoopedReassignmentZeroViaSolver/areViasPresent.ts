454fb06cd8177fa269bbcb374793a6e5280a1df1
seveibar <seveibar@gmail.com>
2025-10-31 20:59:22 -0700

    remove looped reassignment solvers

--- lib/solvers/LoopedReassignmentZeroViaSolver/areViasPresent.ts
+++
@@ -1,7 +0,0 @@
-import { SimplifiedPcbTrace } from "lib/types"
-
-export function areViasPresent(simplifiedPcbTraces: SimplifiedPcbTrace[]) {
-  return simplifiedPcbTraces.some((trace) =>
-    trace.route.some((segment) => segment.route_type === "via"),
-  )
-}
