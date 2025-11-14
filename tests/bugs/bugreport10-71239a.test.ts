da68135488f37d85bf64a1c323455a6d7a16ab3a
0hm☘️ <git8ankanroy@gmail.com>
2025-11-15 01:34:50 +0530

    WIP

--- tests/bugs/bugreport10-71239a.test.ts
+++ tests/bugs/bugreport10-71239a.test.ts
@@ -1,6 +1,8 @@
 import { expect, test } from "bun:test"
 import { AutoroutingPipelineSolver } from "lib"
-import bugReport from "../../examples/bug-reports/bugreport10-71239a/bugreport10-71239a.json" assert { type: "json" }
+import bugReport from "../../examples/bug-reports/bugreport10-71239a/bugreport10-71239a.json" assert {
+  type: "json",
+}
 import type { SimpleRouteJson } from "lib/types"
 
 const srj = bugReport.simple_route_json as SimpleRouteJson
