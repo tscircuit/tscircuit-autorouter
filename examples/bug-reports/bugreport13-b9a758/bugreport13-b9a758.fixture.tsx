ef27b606f08bd59e3d47ff302253ed049015c5d2
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-11-25 02:33:41 +0000

    bug report added

---
+++ examples/bug-reports/bugreport13-b9a758/bugreport13-b9a758.fixture.tsx
@@ -0,0 +1,7 @@
+
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger";
+import bugReportJson from "./bugreport13-b9a758.json";
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />;
+};
