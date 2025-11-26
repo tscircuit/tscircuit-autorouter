248e46a5296f8c8d21b8c5e84a78b53f311492e4
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-11-26 23:38:16 +0000

    format

--- examples/bug-reports/bugreport16-d95f38/bugreport16-d95f38.fixture.tsx
+++ examples/bug-reports/bugreport16-d95f38/bugreport16-d95f38.fixture.tsx
@@ -1,7 +1,6 @@
-
 // @ts-nocheck
-import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger";
-import bugReportJson from "./bugreport16-d95f38.json";
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport16-d95f38.json"
 export default () => {
-  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />;
-};
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
