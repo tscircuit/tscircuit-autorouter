27fd914f5b4964c04943d5d7072d4d0af01a67e3
0hm☘️ <git8ankanroy@gmail.com>
2025-12-06 17:01:12 +0530

    chore: format code

--- examples/legacy/end-to-end/e2e1.fixture.tsx
+++ examples/legacy/end-to-end/e2e1.fixture.tsx
@@ -2,7 +2,6 @@ import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebu
 import { SimpleRouteJson } from "lib/types"
 import keyboardRoutes from "../../legacy/assets/growing-grid-keyboard-sample-sample95-unrouted_simple_route.json"
 
-
 export default () => {
   return <AutoroutingPipelineDebugger srj={keyboardRoutes as SimpleRouteJson} />
 }
