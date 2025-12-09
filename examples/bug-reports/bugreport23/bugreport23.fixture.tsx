61d984746af9986e076a09d2dc41964b19ea52c2
0hm☘️ <git8ankanroy@gmail.com>
2025-12-09 11:32:14 +0530

    feat: Add bugreport23 example for bug reproduction

---
+++ examples/bug-reports/bugreport23/bugreport23.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport23.srj.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson} />
+}
