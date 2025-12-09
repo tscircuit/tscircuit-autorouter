01f0f7d2e06e7b28fc610c1e813a736bfc49692d
0hm☘️ <git8ankanroy@gmail.com>
2025-12-09 11:35:32 +0530

    refactor: Rename bugreport23 to bugreport23-LGA15x4

--- examples/bug-reports/bugreport23-LGA15x4/bugreport23-LGA15x4.fixture.tsx
+++ examples/bug-reports/bugreport23-LGA15x4/bugreport23-LGA15x4.fixture.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
-import bugReportJson from "./bugreport23.srj.json"
+import bugReportJson from "./bugreport23-LGA15x4.srj.json"
 
 export default () => {
   return <AutoroutingPipelineDebugger srj={bugReportJson} />
