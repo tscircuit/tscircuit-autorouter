fcd63e1f7335dc4da76bee1f08659836f80ee420
0hm☘️ <git8ankanroy@gmail.com>
2025-12-13 09:55:28 +0530

    added bugreport24-05597c

---
+++ tests/bugs/bugreport24-05597c.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test"
+import { AutoroutingPipelineSolver } from "lib"
+import bugReport from "../../examples/bug-reports/bugreport24-05597c/bugreport24-05597c.json" assert {
+  type: "json",
+}
+import type { SimpleRouteJson } from "lib/types"
+import { getLastStepSvg } from "../fixtures/getLastStepSvg"
+
+const srj = bugReport.simple_route_json as SimpleRouteJson
+
+test("bugreport24-05597c.json", () => {
+  const solver = new AutoroutingPipelineSolver(srj)
+  solver.solve()
+  expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
+    import.meta.path,
+  )
+})
