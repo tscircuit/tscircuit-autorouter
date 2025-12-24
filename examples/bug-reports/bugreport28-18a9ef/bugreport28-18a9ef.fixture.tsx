ad6dae22c52a85d15f5291d8c9e2e1bb495caf19
0hm☘️ <git8ankanroy@gmail.com>
2025-12-24 19:10:20 +0530

    WIP

--- examples/bug-reports/bugreport28-18a9ef/bugreport28-18a9ef.fixture.tsx
+++ examples/bug-reports/bugreport28-18a9ef/bugreport28-18a9ef.fixture.tsx
@@ -1,7 +1,6 @@
-
 // @ts-nocheck
-import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger";
-import bugReportJson from "./bugreport28-18a9ef.json";
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport28-18a9ef.json"
 export default () => {
-  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />;
-};
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
