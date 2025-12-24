b4411214c4d9249e7421a599b1beb9360ccf22aa
seveibar <seveibar@gmail.com>
2025-12-24 11:29:09 -0800

    add bugreport29

---
+++ examples/bug-reports/bugreport29-7deae8/bugreport29-7deae8.fixture.tsx
@@ -0,0 +1,7 @@
+
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger";
+import bugReportJson from "./bugreport29-7deae8.json";
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />;
+};
---
+++ tests/bugs/bugreport29-7deae8.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "bun:test"
+import { AssignableAutoroutingPipeline2 } from "lib/autorouter-pipelines/AssignableAutoroutingPipeline2/AssignableAutoroutingPipeline2"
+import bugReport from "../../examples/bug-reports/bugreport29-7deae8/bugreport29-7deae8.json" assert {
+  type: "json",
+}
+import type { SimpleRouteJson } from "lib/types"
+import { getLastStepSvg } from "../fixtures/getLastStepSvg"
+
+const srj = bugReport.simple_route_json as SimpleRouteJson
+
+test(
+  "bugreport29-7deae8",
+  () => {
+    const solver = new AssignableAutoroutingPipeline2(srj)
+    solver.solve()
+    expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
+      import.meta.path,
+    )
+  },
+  { timeout: 60_000 },
+)
