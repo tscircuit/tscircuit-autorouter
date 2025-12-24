d918f6e3195ac4da4351398c33af7de1f7b0b7d1
seveibar <seveibar@gmail.com>
2025-12-24 11:31:42 -0800

    format

--- examples/bug-reports/bugreport29-7deae8/bugreport29-7deae8.fixture.tsx
+++ examples/bug-reports/bugreport29-7deae8/bugreport29-7deae8.fixture.tsx
@@ -1,7 +1,6 @@
-
 // @ts-nocheck
-import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger";
-import bugReportJson from "./bugreport29-7deae8.json";
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport29-7deae8.json"
 export default () => {
-  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />;
-};
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
