75c9365f2f953bed1b2ce0def586a39da1ad85c3
seveibar <seveibar@gmail.com>
2026-01-02 22:10:47 -0800

    new jumper-high-density04 example

---
+++ fixtures/features/jumper-high-density/jumper-high-density04.fixture.tsx
@@ -0,0 +1,17 @@
+import { GenericSolverDebugger } from "lib/testing/GenericSolverDebugger"
+import { JumperHighDensitySolver } from "lib/autorouter-pipelines/AssignableAutoroutingPipeline2/JumperHighDensitySolver"
+import input from "./jumper-high-density04-input.json"
+
+export default () => {
+  const createSolver = () => {
+    return new JumperHighDensitySolver({
+      nodePortPoints: input[0].nodePortPoints as any,
+      colorMap: input[0].colorMap,
+      traceWidth: input[0].traceWidth,
+      viaDiameter: input[0].viaDiameter,
+      connMap: input[0].connMap as any,
+    })
+  }
+
+  return <GenericSolverDebugger createSolver={createSolver} />
+}
