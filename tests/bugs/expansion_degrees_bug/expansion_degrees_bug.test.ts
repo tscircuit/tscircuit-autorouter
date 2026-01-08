29d9094e3f520e4bf8567cff1e646225e6df6291
0hm☘️ <git8ankanroy@gmail.com>
2026-01-08 11:59:23 +0530

    WIP

--- tests/bugs/expansion_degrees_bug/expansion_degrees_bug.test.ts
+++ tests/bugs/expansion_degrees_bug/expansion_degrees_bug.test.ts
@@ -4,15 +4,24 @@ import input19 from "./expansion_degrees_bug_bugreport19.json" with {
 import input1 from "./expansion_degrees_bug_bugreport1.json" with {
   type: "json",
 }
+import input16 from "./expansion_degrees_bug_bugreport16.json" with {
+  type: "json",
+}
 import { expect, test } from "bun:test"
-import { getSvgFromGraphicsObject } from "graphics-debug"
+import {
+  getSvgFromGraphicsObject,
+  GraphicsObject,
+  mergeGraphics,
+  stackGraphicsVertically,
+  Text,
+} from "graphics-debug"
 import {
   MultiSectionPortPointOptimizer,
   type MultiSectionPortPointOptimizerParams,
 } from "lib/solvers/MultiSectionPortPointOptimizer"
 import type { PortPoint } from "lib/types/high-density-types"
 
-const EXPANSION_DEGREE = 1
+const EXPANSION_DEGREEs = [1, 2, 3, 4, 5, 6]
 
 type SerializedAssignedPortPoint = {
   connectionName: string
@@ -44,46 +53,128 @@ function deserializeParams(
   }
 }
 
-test("expansion degree 1 snapshot bugreport19", () => {
+test("expansion degree " +
+  EXPANSION_DEGREEs.join(",") +
+  "snapshot bugreport19", () => {
   const [serializedParams] = input19 as unknown as SerializedParams[]
-  const optimizer = new MultiSectionPortPointOptimizer(
-    deserializeParams(serializedParams),
-  )
+  let graphics: GraphicsObject[] = []
 
-  const originalCreateSection = optimizer.createSection.bind(optimizer)
-  optimizer.createSection = (params) =>
-    originalCreateSection({
-      ...params,
-      expansionDegrees: EXPANSION_DEGREE,
-    })
+  for (const EXPANSION_DEGREE of EXPANSION_DEGREEs) {
+    const optimizer = new MultiSectionPortPointOptimizer(
+      deserializeParams(serializedParams),
+    )
 
-  optimizer.solve()
+    const originalCreateSection = optimizer.createSection.bind(optimizer)
+    optimizer.createSection = (params) =>
+      originalCreateSection({
+        ...params,
+        expansionDegrees: EXPANSION_DEGREE,
+      })
 
-  const svg = getSvgFromGraphicsObject(optimizer.visualize(), {
+    optimizer.solve()
+    const rects = optimizer.visualize().rects ?? []
+    let maxY = -Infinity
+    for (const rect of rects) {
+      const top = rect.center.y + rect.height * (2 / 3)
+      if (top > maxY) {
+        maxY = top
+      }
+    }
+    const text: Text = {
+      text: "EXPANSION_DEGREE= " + EXPANSION_DEGREE,
+      x: 0,
+      y: maxY,
+      fontSize: 0.5,
+    }
+    graphics.push(mergeGraphics(optimizer.visualize(), { texts: [text] }))
+  }
+  const viz = stackGraphicsVertically(graphics)
+  const svg = getSvgFromGraphicsObject(viz, {
     backgroundColor: "white",
   })
-
   expect(svg).toMatchSvgSnapshot(import.meta.path + "19")
 })
 
-test("expansion degree 1 snapshot bugreport1", () => {
+test("expansion degree " +
+  EXPANSION_DEGREEs.join(",") +
+  "snapshot bugreport1", () => {
   const [serializedParams] = input1 as unknown as SerializedParams[]
-  const optimizer = new MultiSectionPortPointOptimizer(
-    deserializeParams(serializedParams),
-  )
+  const graphics: GraphicsObject[] = []
+  for (const EXPANSION_DEGREE of EXPANSION_DEGREEs) {
+    const optimizer = new MultiSectionPortPointOptimizer(
+      deserializeParams(serializedParams),
+    )
 
-  const originalCreateSection = optimizer.createSection.bind(optimizer)
-  optimizer.createSection = (params) =>
-    originalCreateSection({
-      ...params,
-      expansionDegrees: EXPANSION_DEGREE,
-    })
+    const originalCreateSection = optimizer.createSection.bind(optimizer)
+    optimizer.createSection = (params) =>
+      originalCreateSection({
+        ...params,
+        expansionDegrees: EXPANSION_DEGREE,
+      })
 
-  optimizer.solve()
-
-  const svg = getSvgFromGraphicsObject(optimizer.visualize(), {
+    optimizer.solve()
+    const rects = optimizer.visualize().rects ?? []
+    let maxY = -Infinity
+    for (const rect of rects) {
+      const top = rect.center.y + rect.height * (2 / 3)
+      if (top > maxY) {
+        maxY = top
+      }
+    }
+    const text: Text = {
+      text: "EXPANSION_DEGREE= " + EXPANSION_DEGREE,
+      x: 0,
+      y: maxY,
+      fontSize: 0.5,
+    }
+    graphics.push(mergeGraphics(optimizer.visualize(), { texts: [text] }))
+  }
+  const viz = stackGraphicsVertically(graphics)
+  const svg = getSvgFromGraphicsObject(viz, {
     backgroundColor: "white",
   })
 
   expect(svg).toMatchSvgSnapshot(import.meta.path + "1")
+})
+
+test("expansion degree " +
+  EXPANSION_DEGREEs.join(",") +
+  "snapshot bugreport16", () => {
+  const [serializedParams] = input16 as unknown as SerializedParams[]
+  const graphics: GraphicsObject[] = []
+  for (const EXPANSION_DEGREE of EXPANSION_DEGREEs) {
+    const optimizer = new MultiSectionPortPointOptimizer(
+      deserializeParams(serializedParams),
+    )
+
+    const originalCreateSection = optimizer.createSection.bind(optimizer)
+    optimizer.createSection = (params) =>
+      originalCreateSection({
+        ...params,
+        expansionDegrees: EXPANSION_DEGREE,
+      })
+
+    optimizer.solve()
+    const rects = optimizer.visualize().rects ?? []
+    let maxY = -Infinity
+    for (const rect of rects) {
+      const top = rect.center.y + rect.height * (2 / 3)
+      if (top > maxY) {
+        maxY = top
+      }
+    }
+    const text: Text = {
+      text: "EXPANSION_DEGREE= " + EXPANSION_DEGREE,
+      x: 0,
+      y: maxY,
+      fontSize: 0.5,
+    }
+    graphics.push(mergeGraphics(optimizer.visualize(), { texts: [text] }))
+  }
+  const viz = stackGraphicsVertically(graphics)
+  const svg = getSvgFromGraphicsObject(viz, {
+    backgroundColor: "white",
+  })
+
+  expect(svg).toMatchSvgSnapshot(import.meta.path + "16")
 })
