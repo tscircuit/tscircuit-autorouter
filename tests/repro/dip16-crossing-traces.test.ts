c10bb16fedb2ce385b4ab90654d070854c0ae555
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-01-10 00:32:51 +0200

    fix type error

--- tests/repro/dip16-crossing-traces.test.ts
+++ tests/repro/dip16-crossing-traces.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "bun:test"
 import reproJson from "./dip16-crossing-traces.json"
 import type { SimpleRouteJson } from "lib/types"
 import { getLastStepSvg } from "../fixtures/getLastStepSvg"
-import { AssignableAutoroutingPipeline3 } from "dist"
+import { AssignableAutoroutingPipeline3 } from "lib/index"
 
 test("dip16 crossing traces", () => {
   const solver = new AssignableAutoroutingPipeline3(
