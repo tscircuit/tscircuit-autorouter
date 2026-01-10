921feab2194ffd13b1859c8a6af4086c5ff2d386
seveibar <seveibar@gmail.com>
2026-01-09 16:55:17 -0800

    gamebody bug report

---
+++ fixtures/bug-reports/bugreport43-e0f33a/bugreport43-e0f33a.fixture.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport43-e0f33a.json"
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
---
+++ tests/bugs/bugreport43-e0f33a.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "bun:test"
+import { AssignableAutoroutingPipeline3 } from "lib"
+import bugReport from "../../fixtures/bug-reports/bugreport43-e0f33a/bugreport43-e0f33a.json" with {
+  type: "json",
+}
+import type { SimpleRouteJson } from "lib/types"
+import { getLastStepSvg } from "../fixtures/getLastStepSvg"
+
+const srj = bugReport.simple_route_json as SimpleRouteJson
+
+test(
+  "bugreport43-e0f33a",
+  () => {
+    const solver = new AssignableAutoroutingPipeline3(srj)
+    solver.solve()
+    expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
+      import.meta.path,
+    )
+  },
+  { timeout: 180_000 },
+)
