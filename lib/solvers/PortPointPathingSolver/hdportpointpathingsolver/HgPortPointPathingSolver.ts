3988f0fae0f3bf7d6901df53014d210ba6abcf39
0hm☘️ <git8ankanroy@gmail.com>
2026-02-13 18:45:12 +0530

    format

--- lib/solvers/PortPointPathingSolver/hdportpointpathingsolver/HgPortPointPathingSolver.ts
+++ lib/solvers/PortPointPathingSolver/hdportpointpathingsolver/HgPortPointPathingSolver.ts
@@ -233,10 +233,7 @@ export class HgPortPointPathingSolver extends HyperGraphSolver<
     const nodeRipCount = this.nodeRipCountMap.get(nodeId) ?? 0
     const nodeRipFraction = Math.min(1, nodeRipCount / this.maxNodeRips)
     const startRippingPfThreshold = this.ripNodePfThresholdStart
-    return (
-      startRippingPfThreshold * nodeRipFraction +
-      1 * (1 - nodeRipFraction)
-    )
+    return startRippingPfThreshold * nodeRipFraction + 1 * (1 - nodeRipFraction)
   }
 
   private getPortPointsFromRegionAssignments(
