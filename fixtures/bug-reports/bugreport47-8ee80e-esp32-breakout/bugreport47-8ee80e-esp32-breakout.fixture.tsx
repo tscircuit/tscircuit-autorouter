d83843b53d42cefa169f5fc048affe7a1009cc2e
Severin Ibarluzea <seve700@gmail.com>
2026-03-17 11:23:37 -0700

    Add esp32 breakout bug report fixture and regression test

---
+++ fixtures/bug-reports/bugreport47-8ee80e-esp32-breakout/bugreport47-8ee80e-esp32-breakout.fixture.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport47-8ee80e-esp32-breakout.json"
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
