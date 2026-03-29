fc29d59fe8e84d34f6ed8e7aa0a76a1933f27b9c
SyedHannanMehdi <47053176+SyedHannanMehdi@users.noreply.github.com>
2026-03-30 02:31:50 +0530

    feat: re-export trace-thickness types from lib/types index

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,8 +1,1 @@
-export * from "srj-types/capacity-mesh-types";
-export * from "srj-types/capacity-obstacle-types";
-export * from "srj-types/capacity-route-types";
-
-export * from "./SimpleRouteJson";
-export * from "./AutorouterResult";
-export * from "./high-density-types";
-export * from "./TraceThickness";
+export * from "./trace-thickness"
