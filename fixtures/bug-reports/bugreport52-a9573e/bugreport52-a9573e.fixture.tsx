a00f682de536ee099d2678072e2dfe0358e2d5cc
imrishabh18 <belikerishabh@gmail.com>
2026-04-25 15:55:35 -0700

    Add the bug-report-52

---
+++ fixtures/bug-reports/bugreport52-a9573e/bugreport52-a9573e.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport52-a9573e.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
