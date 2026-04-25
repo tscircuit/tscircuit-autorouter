8b4e8fece0a754a0ad04f4623106dc60ff6168b3
imrishabh18 <belikerishabh@gmail.com>
2026-04-25 16:57:06 -0700

    Add bug report 53

---
+++ fixtures/bug-reports/bugreport53-8e0eba/bugreport53-8e0eba.fixture.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport53-8e0eba.json"
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
