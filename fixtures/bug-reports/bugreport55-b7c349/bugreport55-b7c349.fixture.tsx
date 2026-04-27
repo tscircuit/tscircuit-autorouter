7aeda62c24decb34d16a0c3302a48db6ec09f73f
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-04-27 15:18:02 -0700

    Add bugreport55-b7c349 fixture

---
+++ fixtures/bug-reports/bugreport55-b7c349/bugreport55-b7c349.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport55-b7c349.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
