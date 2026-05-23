99db3624f2e852d0359ef40e05878c48047dd2cc
Rohit <dv6510@gmail.com>
2026-05-23 21:11:13 +0530

    test: remove timeout from bugreport04 test

--- tests/bugs/bugreport04-aa1d41.test.ts
+++ tests/bugs/bugreport04-aa1d41.test.ts
@@ -6,14 +6,10 @@ import { getLastStepSvg } from "tests/fixtures/getLastStepSvg"
 
 const srj = bugReport.simple_route_json as SimpleRouteJson
 
-test(
-  "bugreport04-aa1d41.json-AutoroutingPipeline1_OriginalUnravel",
-  () => {
-    const solver = new AutoroutingPipeline1_OriginalUnravel(srj)
-    solver.solve()
-    expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
-      import.meta.path,
-    )
-  },
-  { timeout: 30000 },
-)
+test("bugreport04-aa1d41.json-AutoroutingPipeline1_OriginalUnravel", () => {
+  const solver = new AutoroutingPipeline1_OriginalUnravel(srj)
+  solver.solve()
+  expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
+    import.meta.path,
+  )
+})
