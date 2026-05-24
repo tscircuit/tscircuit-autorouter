4012eee1604aa30c962c15287dbbeb22d852bf4e
Severin Ibarluzea <seveibar@gmail.com>
2026-05-24 12:02:04 -0700

    Add QFP thermalpad example (#1280)

---
+++ fixtures/bug-reports/bugreport63-274be2/bugreport63-274be2.fixture.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport63-274be2.json"
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
---
+++ tests/bugs/bugreport63-274be2.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test"
+import { AutoroutingPipelineSolver } from "lib"
+import bugReport from "../../fixtures/bug-reports/bugreport63-274be2/bugreport63-274be2.json" with {
+  type: "json",
+}
+import type { SimpleRouteJson } from "lib/types"
+import { getLastStepSvg } from "../fixtures/getLastStepSvg"
+
+const srj = bugReport.simple_route_json as SimpleRouteJson
+
+test("bugreport63-274be2.json", () => {
+  const solver = new AutoroutingPipelineSolver(srj)
+  solver.solve()
+  expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
+    import.meta.path,
+  )
+})
