fc7d3145fb8184aa18a26074f005ce25a4379ea0
Majira <JirA44@users.noreply.github.com>
2026-06-07 13:18:51 +0200

    fix: address review feedback for #66

--- fixtures/bug-reports/bugreport01-be84eb/bugreport01-be84eb.fixture.tsx
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
-import bugReportJson from "./bugreport01-be84eb.json"
-
-export default () => {
-  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
-}
