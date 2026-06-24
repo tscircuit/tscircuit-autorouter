06492bd5e918e35996c29ac9b6d26e793f48251e
imrishabh18 <belikerishabh@gmail.com>
2026-06-24 22:03:48 +0530

    Bugreport 66

---
+++ fixtures/bug-reports/bugreport66-e6c7e0/bugreport66-e6c7e0.fixture.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport66-e6c7e0.json"
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
