059a75f17152650fd142eff6a16aef42eec5e36b
0hm☘️ <git8ankanroy@gmail.com>
2026-07-02 23:37:14 +0530

    Add solar battery charger snapshot bugreport

---
+++ fixtures/bug-reports/bugreport68-solar-battery-charger/bugreport68-solar-battery-charger.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import srj from "./bugreport68-solar-battery-charger.srj.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={srj} />
+}
---
+++ tests/bugs/bugreport68-solar-battery-charger.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test"
+import { AutoroutingPipelineSolver7_MultiGraph } from "lib"
+import type { SimpleRouteJson } from "lib/types"
+import srj from "../../fixtures/bug-reports/bugreport68-solar-battery-charger/bugreport68-solar-battery-charger.srj.json" with {
+  type: "json",
+}
+import { getLastStepSvg } from "../fixtures/getLastStepSvg"
+
+const simpleRouteJson = srj as SimpleRouteJson
+
+test("bugreport68-solar-battery-charger.srj.json", () => {
+  const solver = new AutoroutingPipelineSolver7_MultiGraph(simpleRouteJson)
+  solver.solveUntilPhase("escapeViaLocationSolver")
+  expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
+    import.meta.path,
+  )
+})
