1ae3d80977ddfae8842cb86e73731b1a202ef99c
0hm☘️ <git8ankanroy@gmail.com>
2026-07-04 04:27:56 +0530

    add bugreport68 regression test

---
+++ tests/bugs/bugreport68-41562e.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test"
+import { AutoroutingPipelineSolver7_MultiGraph } from "lib/autorouter-pipelines/AutoroutingPipeline7_MultiGraph/AutoroutingPipelineSolver7_MultiGraph"
+import bugReport from "../../fixtures/bug-reports/bugreport68-41562e/bugreport68-41562e.json" with {
+  type: "json",
+}
+import type { SimpleRouteJson } from "lib/types"
+import { getLastStepSvg } from "../fixtures/getLastStepSvg"
+
+const srj = bugReport.simple_route_json as SimpleRouteJson
+
+test("bugreport68-41562e.json", () => {
+  const solver = new AutoroutingPipelineSolver7_MultiGraph(srj)
+  solver.solve()
+  expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
+    import.meta.path,
+  )
+})
