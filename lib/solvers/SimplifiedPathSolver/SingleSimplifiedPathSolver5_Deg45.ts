5b5d7e08f5b2d2634aa1b68fcb828411242ac0e3
0hm☘️ <git8ankanroy@gmail.com>
2026-07-05 01:54:12 +0530

    Clamp route distance lookup at final segment

--- lib/solvers/SimplifiedPathSolver/SingleSimplifiedPathSolver5_Deg45.ts
+++ lib/solvers/SimplifiedPathSolver/SingleSimplifiedPathSolver5_Deg45.ts
@@ -373,8 +373,12 @@ export class SingleSimplifiedPathSolver5 extends SingleSimplifiedPathSolver {
     // If closer to the end of the segment, return the next index
     const segment = this.pathSegments[segmentIndex]
     const midDistance = (segment.startDistance + segment.endDistance) / 2
-
-    return distance > midDistance ? segmentIndex + 1 : segmentIndex
+    const currentSegmentIndex = segmentIndex
+    let nextSegmentIndex = segmentIndex + 1
+    if (nextSegmentIndex >= this.pathSegments.length) {
+      nextSegmentIndex = currentSegmentIndex
+    }
+    return distance > midDistance ? nextSegmentIndex : currentSegmentIndex
   }
 
   // Check if a path segment is valid
