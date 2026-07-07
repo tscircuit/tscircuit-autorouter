80292914bf2b80a97c393316979c1bcbc426b226
0hm☘️ <git8ankanroy@gmail.com>
2026-07-07 23:47:43 +0530

    add bug report bugreport71-dd7d15

---
+++ tests/bugs/bugreport71-dd7d15.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test"
+import { AutoroutingPipelineSolver } from "lib"
+import bugReport from "../../fixtures/bug-reports/bugreport71-dd7d15/bugreport71-dd7d15.json" with {
+  type: "json",
+}
+import type { SimpleRouteJson } from "lib/types"
+import { getLastStepSvg } from "../fixtures/getLastStepSvg"
+
+const srj = bugReport.simple_route_json as SimpleRouteJson
+
+test("bugreport71-dd7d15.json", () => {
+  const solver = new AutoroutingPipelineSolver(srj)
+  solver.solve()
+  expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
+    import.meta.path,
+  )
+})
