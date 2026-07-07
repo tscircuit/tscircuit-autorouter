102f60ccf43d50def2075732a50c2ce47b97cf91
0hm☘️ <109351887+0hmX@users.noreply.github.com>
2026-07-07 11:11:13 +0530

    add pipeline7 failing route cosmos fixtures (#1512)
    * add pipeline7 failing route snapshot repros
    
    * move pipeline7 repros to cosmos fixtures
    
    * use pipeline debugger for repro fixtures

---
+++ fixtures/bug-reports/bugreport69-5a6a68/bugreport69-5a6a68.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import phaseInputs from "./bugreport69-5a6a68.phase-inputs.srj.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={phaseInputs[0]} />
+}
---
+++ fixtures/bug-reports/bugreport70-4e510c/bugreport70-4e510c.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import phaseInputs from "./bugreport70-4e510c.phase-inputs.srj.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={phaseInputs[0]} />
+}
