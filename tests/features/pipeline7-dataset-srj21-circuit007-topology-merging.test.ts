31e4c14c2ae72c50886476a3cdc2f6d97f3bb96a
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-07-11 12:14:46 +0200

    remove redundant tests

--- tests/features/pipeline7-dataset-srj21-circuit007-topology-merging.test.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "bun:test"
-import { assertPipeline7DatasetSrj21TopologyMerging } from "../fixtures/assert-pipeline7-dataset-srj21-topology-merging"
-
-test("pipeline7 merges topology for dataset-srj21 circuit007", async (): Promise<void> => {
-  await assertPipeline7DatasetSrj21TopologyMerging({
-    sampleNumber: 7,
-    expectedComponentKinds: ["qfp", "soic"],
-  })
-})
