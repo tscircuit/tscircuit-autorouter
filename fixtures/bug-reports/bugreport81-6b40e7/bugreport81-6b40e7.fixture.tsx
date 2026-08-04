ef94d0f05985fd0dc8c5ee596cf829c40c1e94cf
0hm☘️ <git8ankanroy@gmail.com>
2026-08-04 10:43:23 +0530

    Add bug report 6b40e7

---
+++ fixtures/bug-reports/bugreport81-6b40e7/bugreport81-6b40e7.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport81-6b40e7.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />;
+}
