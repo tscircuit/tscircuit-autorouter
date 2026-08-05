ea90b17d8e99da4e13063329aff84eef8041b841
Severin Ibarluzea <seveibar@gmail.com>
2026-08-05 16:14:04 -0700

    Add F1C100S breakout routing bug report (#1933)

---
+++ fixtures/bug-reports/bugreport83-f1c100s-breakout/bugreport83-f1c100s-breakout.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import srj from "./bugreport83-f1c100s-breakout.srj.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={srj} />
+}
