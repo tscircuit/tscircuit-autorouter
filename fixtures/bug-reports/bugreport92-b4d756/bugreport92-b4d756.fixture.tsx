79184b872de9df98fb32aeba34a8540b089c11c2
imrishabh18 <belikerishabh@gmail.com>
2026-08-12 20:31:42 +0530

    Remove redundant bugreport92 regression

--- fixtures/bug-reports/bugreport92-b4d756/bugreport92-b4d756.fixture.tsx
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
-import bugReportJson from "./bugreport92-b4d756.json"
-export default () => {
-  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
-}
