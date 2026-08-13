efa97eb5e1eb885f439ef64100c9ffc3deeeda2c
Mustafa Mulla <mustafamulla765@gmail.com>
2026-08-13 19:29:17 +0530

    Add BOOST-DRV8848 power routing bug report

---
+++ fixtures/bug-reports/bugreport92-951dfd/bugreport92-951dfd.fixture.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport92-951dfd.json"
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
