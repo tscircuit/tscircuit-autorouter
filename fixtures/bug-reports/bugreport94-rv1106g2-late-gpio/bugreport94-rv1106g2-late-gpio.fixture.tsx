16c9e0e6b122c645e142e7cc27a6797af81185df
mohan-bee <mohn08052006@gmail.com>
2026-08-19 23:47:31 +0530

    up

--- fixtures/bug-reports/bugreport94-rv1106g2-late-gpio/bugreport94-rv1106g2-late-gpio.fixture.tsx
+++ fixtures/bug-reports/bugreport94-rv1106g2-late-gpio/bugreport94-rv1106g2-late-gpio.fixture.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
-import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
 import { AutoroutingPipelineSolver9_PreloadedTraceGraph } from "lib"
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
 import srj from "./bugreport94-rv1106g2-late-gpio.srj.json"
 
 export default () => (
