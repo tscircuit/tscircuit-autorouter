a5b2f96e318946e7d5bd8b68a189b069347ebd22
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-08-20 17:29:01 +0530

    Apply suggestion from @imrishabh18

--- tests/bugs/bugreport94-56fa2e.test.ts
+++ tests/bugs/bugreport94-56fa2e.test.ts
@@ -17,7 +17,7 @@ test("bugreport94-56fa2e.json", () => {
   const circuitJson = getCurrentCircuitJson(solver)
   expect(circuitJson).not.toBeNull()
   const { errors } = getDrcErrors(circuitJson!)
-  expect(errors).toHaveLength(45)
+  expect(errors).toHaveLength(14)
 
   expect(getLastStepSvg(solver.visualize())).toMatchSvgSnapshot(
     import.meta.path,
