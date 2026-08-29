0b6b8a2dbcca097dfe9d6b97cf490bc975b2c715
seveibar <seveibar@gmail.com>
2026-08-28 21:36:59 -0700

    Revert "feat: add Pipeline9 networked high-density cache (#2269)"
    This reverts commit 725a1588e11d1e6252f0c7b2bdf867a647c2c044.

--- tests/features/pipeline9-networked-version.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "bun:test"
-import { AUTOROUTER_VERSION } from "lib"
-import packageJson from "../../package.json" with { type: "json" }
-
-test("Pipeline9 network cache version matches the package version", () => {
-  expect(AUTOROUTER_VERSION).toBe(packageJson.version)
-  expect(packageJson.scripts.prepack).toBe("bun run build")
-})
